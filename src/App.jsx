478d165e08960ff9185a5522718c6dc500bc0705
Jeremy <jerimonski@gmail.com>
2025-08-12 10:45:18 -0400

    feat: section General  Status

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import GeneralStatusCards from "./components/GeneralStatusCards"
+import { CiWarning } from "react-icons/ci"
+import { MdOnlinePrediction } from "react-icons/md"
+import { ImConnection } from "react-icons/im"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="mx-auto w-[1120px]">
+      <section>
+        <article className="bg-amber-500 ">
+          <h2 className="text-3xl">Estado de red</h2>
+          <div className="flex flex-grow justify-between">
+            <span className="text-sm">Monitoreo de ip en tiempo real</span>
+            <button>Descarga Excel</button>
+          </div>
+        </article>
+        <div className="flex flex-grow justify-between gap-6">
+          <GeneralStatusCards
+            BorderColor={"border-green-500"}
+            TextColor={"text-green-500"}
+            Title={"Online"}
+            ValueInNetwork={123}
+            Icon={<ImConnection size={45} color="LimeGreen" />}
+          />
+          <GeneralStatusCards
+            BorderColor={"border-red-600"}
+            TextColor={"text-red-500"}
+            Title={"OffLine"}
+            ValueInNetwork={123}
+            Icon={<CiWarning size={45} color="red" />}
+          />
+          <GeneralStatusCards
+            BorderColor={"border-blue-400"}
+            TextColor={"text-blue-500"}
+            Title={"total IP's"}
+            ValueInNetwork={123}
+            Icon={<MdOnlinePrediction size={45} color="DodgerBlue" />}
+          />
+        </div>
+      </section>
+    </main>
   )
 }
-
-export default App
