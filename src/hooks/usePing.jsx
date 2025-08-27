3147051e39dcd1dab993914371f8d7b3147dc7a2
Jeremy <jerimonski@gmail.com>
2025-08-27 11:47:32 -0400

    fix: deleting a console.log

--- src/hooks/usePing.jsx
+++ src/hooks/usePing.jsx
@@ -18,7 +18,6 @@ export default function usePing() {
     // el parametro data contiene toda la informacion que recibe
     // del backend, y accede a ellos con el .devices
     socket.on("status_update", (data) => {
-      console.log(data)
       if (data && Array.isArray(data.devices)) {
         setPingData(data.devices)
         setConnectionStatus("Datos recibidos.")
